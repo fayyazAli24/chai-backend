c779c2d67d597311bbf55e988693ef6c599e094a
AwaisQazii <awaisqazi6391@gmail.com>
2024-04-27 19:08:20 +0500

    user auth complete with session tokens and refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { upload } from "../middlewares/multer.middleware.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 const registerUser = asyncHandler(async (req, res) => {
+  
   // get user detail from frontend
   // validation - empty
   // check if user already exist through email
@@ -15,6 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // remove password and refresh token in user response
   // check for user creation
   // return response
+
 
 
   // getting the parameters from the req body 
@@ -102,4 +104,174 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req,res)=>{
+  // req body -> data
+  // username email
+  // find user
+  // check password
+  // access token and refresh token
+  // send cookie
+
+  const {email,userName,password} = req.body;
+
+  console.log("the email is ",email);
+
+  if(!userName && !email){
+    throw new ApiError(400,"password and email is required")
+  }
+
+  const user = await User.findOne({
+    $or: [{email} ,{userName}]
+  })
+
+  if(!user){
+    throw new ApiError(404,"User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if(!isPasswordValid){
+    throw new ApiError(404,"invalid user credentials");
+  }
+
+  const { accessToken , refreshToken } =await generateAccessAndRefreshToken(user._id);
+
+
+  // either we get the updated user from DB whose refresh token is updated
+  // or we can update the old object it all depends on you
+  const loggedInUser = await User.findById(user._id).
+  select("-password -refreshToken");
+
+  // now configuring the cookie behaviour
+  // now this cookie can only be modified by the server not the frontend
+  const option = {
+    httpOnly:true,
+    secure:true
+  }
+
+  return res.status(200)
+  .cookie("accessToken",accessToken,option)
+  .cookie("refreshToken",refreshToken,option)
+  .json(
+    new ApiResponse(
+      200,
+      {
+       user: loggedInUser,accessToken,refreshToken
+      },
+      "User logged In successfully"
+    )
+  )
+
+});
+
+const logoutUser = asyncHandler(async (req,res)=>{
+  // first i want to find the user who i want to log out
+  // i made a custom middleware and injected it in my req
+
+  console.log("the user in the logout is ",req.user.email);
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken : undefined
+      }
+    },
+    {
+      new:true
+    }
+  );
+
+  const option = {
+    httpOnly:true,
+    secure:true
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken",option)
+  .clearCookie("refreshToken",option)
+  .json(
+    new ApiResponse(200,{},"User logged out")
+  )
+})
+
+const generateAccessAndRefreshToken = async (userID)=>{
+  try{
+    const user = await User.findById(userID);
+
+    // at this point i have my both access and refresh token
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+
+    // setting the value of refresh token so no need for password everytime
+    user.refreshToken = refreshToken;
+
+    // saved the token in DB
+    await user.save({
+      validateBeforeSave:false
+    });
+
+
+    return {accessToken,refreshToken};
+
+  }catch(e){
+    throw new ApiError(500,"something went wrong while generating the refresh and access token");
+  }
+}
+
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"unauthorized request");
+  }
+
+  try{
+    // now verify incoming token, this will return the user object
+    const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id);
+  
+    if(!user){
+      throw new ApiError(401,"Invalid refresh token")
+    }
+  
+    // now match the user incoming token with the user from the database
+
+    if(incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401,"refresh token is expired");
+    }
+  
+    const option ={
+      httpOnly:true,
+      secure:true
+    }
+  
+    const {updatedAcessToken,updatedRefreshToken} = await generateAccessAndRefreshToken(user._id);
+  
+    return res
+    .status(200)
+    .cookie("accessToken",updatedAcessToken,option)
+    .cookie("refreshToken",updatedRefreshToken,option)
+    .json(
+      new ApiResponse(200,{
+        "updatedAccessToken":updatedAcessToken,
+        "updatedRefreshToken":updatedRefreshToken,
+      },"Access Token refreshed")
+    )
+  }catch(e){
+    throw new ApiError(401,e?.message|| "invalid refresh token")
+  }
+  
+ 
+})
+
+
+
+export { 
+  registerUser,
+  loginUser, 
+  logoutUser,
+  refreshAccessToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js"
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 
 const router = Router();
 
+// for registeration
 router.route("/register").post(
     upload.fields(
         [
@@ -19,6 +23,14 @@ router.route("/register").post(
             }
         ]
     ),
+    
     registerUser)
+
+
+// for user login
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT , logoutUser );
+
 
 export default router;
